9ed5f90508cb92589de0b13517ae2d5ee16b7021
Aidan Stephenson <122556343+asteph03@users.noreply.github.com>
2024-04-23 15:15:36 +0000

    Pending changes exported from your codespace

--- tests/test_course_grader.py
+++ tests/test_course_grader.py
@@ -1,12 +1,38 @@
 import pytest
 from course_grader import convert_to_letter_grade
 
-
 # TODO-1: Add test_exact_grade_boundaries() function
-
+def test_exact_grade_boundaries():
+    print(convert_to_letter_grade(score=0))
+    print(convert_to_letter_grade(score=59))
+    print(convert_to_letter_grade(score=60))
+    print(convert_to_letter_grade(score=69))
+    print(convert_to_letter_grade(score=70))
+    print(convert_to_letter_grade(score=79))
+    print(convert_to_letter_grade(score=80))
+    print(convert_to_letter_grade(score=89))
+    print(convert_to_letter_grade(score=90))
 
 # TODO-2: Add test_invalid_numerical_score() function
-
+def test_invalid_numerical_score():
+    if (convert_to_letter_grade(score=-1) == ValueError("Score must be between 0 and 100.")):
+        print(convert_to_letter_grade(score=-1))
+    
+    if (convert_to_letter_grade(score=101) == ValueError("Score must be between 0 and 100.")):
+        print(convert_to_letter_grade(score=101))
 
 # TODO-3: Add test_non_numeric_input() function
+def test_non_numeric_input():
+    str = ""
+    list = [1, 2, 3]
+    n = None
+
+    if (convert_to_letter_grade(str) == TypeError("Score must be a numeric value.")):
+        print(convert_to_letter_grade(str))
+
+    if (convert_to_letter_grade(list) == TypeError("Score must be a numeric value.")):
+        print(convert_to_letter_grade(list))
+
+    if (convert_to_letter_grade(n) == TypeError("Score must be a numeric value.")):
+        print(convert_to_letter_grade(n))
 
