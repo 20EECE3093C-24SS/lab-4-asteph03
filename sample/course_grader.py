0f7829a495f4393d6911d5fd0b0b61cdaf20bb13
Aidan Stephenson <122556343+asteph03@users.noreply.github.com>
2024-04-23 15:19:48 +0000

    modified:   .github/workflows/ci-pytest.yaml 	modified:   .github/workflows/ci-sphinx.yaml 	modified:   README.md 	modified:   sample/course_grader.py 	modified:   tests/test_course_grader.py

--- sample/course_grader.py
+++ sample/course_grader.py
@@ -1,2 +1,19 @@
 # TODO-1: add convert_to_letter_grade(score) function
     
+def convert_to_letter_grade(score):
+    if not type(score) is int:
+        raise TypeError("Score must be a numeric value.")
+    
+    elif score > 100 or score < 0:
+        raise ValueError("Score must be between 0 and 100.")
+    
+    elif score >= 90:
+        return 'A'
+    elif score >= 80:
+        return 'B'
+    elif score >= 70:
+        return 'C'
+    elif score >= 60:
+        return 'D'
+    else:
+        return 'F'
